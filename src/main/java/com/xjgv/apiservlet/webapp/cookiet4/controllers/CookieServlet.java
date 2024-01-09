2dbff9a9ba64f32d4f1c9d23c0621d6bf6c41c20
Javier Gomez <javier.gov@outlook.es>
2024-01-08 23:16:46 -0600

    creacion de cookie

--- src/main/java/com/xjgv/apiservlet/webapp/cookiet4/controllers/CookieServlet.java
+++ src/main/java/com/xjgv/apiservlet/webapp/cookiet4/controllers/CookieServlet.java
@@ -8,12 +8,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 
 @WebServlet("/cambiar-color")
 public class CookieServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+        Cookie[] cookies = req.getCookies() != null ? req.getCookies() : new Cookie[0];
+        Optional<String> cookieOptional = Arrays.stream(cookies)
+                .filter(c -> "color".equals(c.getName()))
+                .map(Cookie::getValue)
+                .findAny();
     }
 }
